64b0d3ad8cfdcc920ba0149f6711db4a56b1efff
Christian Fernandez <cfernandezm1700@alumno.ipn.mx>
2025-09-03 17:03:55 -0600

    Subiendo Proyecto a Github

--- app/src/main/java/com/example/tarea1/MainActivity.kt
+++ app/src/main/java/com/example/tarea1/MainActivity.kt
@@ -1,4 +1,24 @@
 package com.example.tarea1
 
-class MainActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Cargar un fragment simple por defecto
+        if (savedInstanceState == null) {
+            loadFragment(SimpleFragment())
+        }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
 }
--- app/src/main/java/com/example/tarea1/SimpleFragment.kt
+++ app/src/main/java/com/example/tarea1/SimpleFragment.kt
@@ -1,4 +1,17 @@
 package com.example.tarea1
 
-class SimpleFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class SimpleFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_simple, container, false)
+    }
 }
